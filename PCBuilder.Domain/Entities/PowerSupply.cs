0ab97f389a0a5d607de285c9e3f88bcee8bc92e4
Nicolás Alurralde <nl18947@gmail.com>
2026-02-19 16:07:32 -0300

    feat: complete physical constraints with PcCase, Cooler, and advanced PowerSupply specs

--- PCBuilder.Domain/Entities/PowerSupply.cs
+++ PCBuilder.Domain/Entities/PowerSupply.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PCBuilder.Domain.Enums;
 
 namespace PCBuilder.Domain.Entities
 {
-    internal class PowerSupply
+    public class PowerSupply : Component
     {
+        public int Wattage { get; set; }
+        public EfficiencyRating Certification { get; set; }
+
+        // Físico y Cables
+        public PsuFormFactor FormFactor { get; set; }
+        public int LengthMm { get; set; }
+
+        // ¡Tu nueva idea!
+        public PsuModularity Modularity { get; set; }
     }
 }
