95bbc85dd513de50b18236ae4cd7088f89b004c7
Nicolás Alurralde <nl18947@gmail.com>
2026-02-20 13:18:51 -0300

    feat: implement generic Repository pattern for data access

--- PCBuilder.API/Program.cs
+++ PCBuilder.API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped(typeof(PCBuilder.Domain.Interfaces.IRepository<>), typeof(PCBuilder.Infrastructure.Repositories.Repository<>));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ PCBuilder.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PCBuilder.Domain.Interfaces
+{
+    // El <T> significa que esta interfaz funcionará para cualquier entidad (Processor, Motherboard, etc.)
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
